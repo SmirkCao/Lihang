dff05081a001cddf37c3ee83420d8260c06b50dd
Smirk Cao <smirk.cao@gmail.com>
2018-06-26 11:34:02 +0800

    GD for LR 不完全版，可以体会下有多慢

---
+++ CH6/logistic_regression.py
@@ -0,0 +1,114 @@
+# -*-coding:utf-8-*-
+# Project: CH6  
+# Filename: logistic_regression
+# Author: 😏 <smirk dot cao at gmail dot com>
+
+import time
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+class LogisticRegression(object):
+
+    def __init__(self,
+                 learning_step = 0.00001,
+                 epsilon=0.001,
+                 n_iter=1500):
+        self.learning_step = learning_step
+        self.epsilon_ = epsilon
+        self.n_iter_ = n_iter
+        self.coef_ = []
+        self.cols_ = []
+
+    def fit(self, x_, y_):
+        return self.gradient_descent(x_, y_, epsilon_=self.epsilon_, n_iter=self.n_iter_)
+
+    def predict(self, x_):
+        rst = np.array([self.cols_[idx] for idx in [np.argmax(rst) for rst in 1 - sigmoid(np.dot(x_, self.coef_.T))]])
+        return rst
+
+    def gradient_descent(self, x_, y_, epsilon_=0.001, n_iter=1500):
+        n = x_.shape[len(x_.shape)-1]
+        f_his = []
+        y_ = pd.get_dummies(y_)
+        w_ = np.array([])
+        print(y_.shape, y_.columns)
+
+        # OvR for multiclass N个分类器 ck vs rest
+        for ck in np.arange(y_.shape[1]):
+            wck_ = np.zeros(n)
+            # k = 0
+            for k in np.arange(n_iter):
+                f_xk = self.f(x_, y_.values[:, ck], wck_)
+                g_k = self.g(x_, y_.values[:, ck], wck_)
+
+                if np.average(g_k*g_k) < epsilon_:
+                    w_ = np.vstack([w_, wck_])
+                else:
+                    p_k = -g_k
+                lambda_k = 0.0000001  # TODO: 更新算法
+                wck_ = wck_ + lambda_k*p_k
+                f_his.append(f_xk)
+            w_ = wck_ if w_.size == 0 else np.vstack([w_, wck_])
+            print("progress:", ck, "done")
+            self.coef_ = w_
+            self.cols_ = y_.columns.tolist()
+        return self.coef_, self.cols_
+
+
+def f(x_, y_, w_):
+    # Logistic Regression Loss
+    m = y_.size
+    rst_ = -(1 / m) * np.sum(np.dot(x_, w_) * y_ - np.log(1 + np.exp(np.dot(x_, w_))))
+    return rst_
+
+
+def g(x_, y_, w_):
+    m = y_.size
+    rst_ = (1 / m) * np.dot(x_.T, y_ * sigmoid(np.dot(x_, w_)))
+    return rst_
+
+
+def sigmoid(x_):
+    p = np.exp(x_)
+    p = p / (1 + p)
+    return p
+
+
+def load_data(path_='./Input/train.csv'):
+    raw_data = pd.read_csv(path_)
+    y = raw_data["label"].values
+    del raw_data["label"]
+    X = raw_data.values
+    return X, y
+
+
+if __name__ == "__main__":
+    print('Start read data')
+    time_1 = time.time()
+    X, y = load_data()
+    X = X[:300]
+    y = y[:300]
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.33, random_state=2018)
+    time_2 = time.time()
+    print('read data cost ', time_2 - time_1, ' second', '\n')
+
+    print('Start training')
+    clf = LogisticRegression()
+    clf.f = f
+    clf.g = g
+    clf.fit(train_x, train_y)
+    time_3 = time.time()
+    print('training cost ', time_3 - time_2, ' second', '\n')
+
+    print('Start predicting')
+    test_predict = clf.predict(test_x)
+    time_4 = time.time()
+    print('predicting cost ', time_4 - time_3, ' second', '\n')
+
+    score = accuracy_score(test_y, test_predict)
+    print("The accruacy socre is ", score)
+
+
---
+++ CH6/unit_test.py
@@ -0,0 +1,88 @@
+# -*-coding:utf-8-*-
+# Project: CH6  
+# Filename: unit_test
+# Author: 😏 <smirk dot cao at gmail dot com>
+
+
+from logistic_regression import *
+
+
+def test_load_data(path_='./Input/train.csv'):
+    X, y = load_data(path_)
+    print(X.shape, y.shape)
+    return X, y
+
+
+def test_grident_decent():
+    x, y = test_load_data()
+
+    x = x[:100]
+    y = y[:100]
+    print(x.shape, y.shape)
+
+    # R2 loss
+    # def f(x_, y_, w_):
+    #     # $L(w)=\frac{1}{2N}\sum_{i=1}^{N}(y-w\cdot x)^2$
+    #     rst = 0
+    #     m = y_.size
+    #     rst = 1 / (2 * m) * np.sum((y_ - np.dot(x_, w_)) ** 2)
+    #     return rst
+    #
+    # def g(x_, y_, w_):
+    #     # $L'(w)=\frac{1}{2}x\cdot(w\cdot x-y)$
+    #     # rst = []
+    #     m = y_.size
+    #     rst = 1 / m * np.dot(x_.T, (np.dot(x_, w_) - y_))
+    #     return rst
+
+    # LR loss
+    # def f(x_, y_, w_):
+    #     # Logistic Regression Loss
+    #     m = y_.size
+    #     rst_ = -(1 / m) * np.sum(np.dot(x_, w_) * y_ - np.log(1 + np.exp(np.dot(x_, w_))))
+    #     return rst_
+    #
+    # def g(x_, y_, w_):
+    #     m = y_.size
+    #     rst_ = (1 / m) * np.dot(x_.T, y_ * sigmoid(np.dot(x_, w_)))
+    #     return rst_
+    #
+    # def sigmoid(x_):
+    #     p = np.exp(x_)
+    #     p = p / (1 + p)
+    #     return p
+
+    clf = LogisticRegression()
+    clf.f = f
+    clf.g = g
+    rst_w, rst_cols = clf.gradient_descent(x, y)
+
+    time_1 = time.time()
+    rst = np.array([rst_cols[idx] for idx in [np.argmax(rst) for rst in 1-sigmoid(np.dot(x, rst_w.T))]])
+    rst = np.vstack([rst, y])
+    time_2 = time.time()
+    print('predict cost ', time_2 - time_1, ' second', '\n')
+
+    print(rst.T)
+
+
+def test_lr():
+    x, y = test_load_data()
+
+    x = x[:100]
+    y = y[:100]
+    print(x.shape, y.shape)
+
+    clf = LogisticRegression()
+    clf.f = f
+    clf.g = g
+    clf.fit(x, y)
+    rst = clf.predict(x,)
+    print(rst)
+
+
+if __name__ == '__main__':
+    # test_lr()
+    # X, y = test_load_data(path_='./Input/train_10.csv')
+    # test_grident_decent()
+    test_lr()
