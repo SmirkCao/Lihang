9920906e05cbd3ef2faf5277ee68d2f6e443aeaa
smirk <smirk.cao@gmail.com>
2018-09-05 11:25:09 +0800

    may it worked, but not so clearly.

--- CH6/maxent.py
+++ CH6/maxent.py
@@ -8,16 +8,20 @@ from sklearn.metrics import accuracy_score
 from collections import defaultdict
 import pandas as pd
 import numpy as np
+import argparse
 import time
 import logging
 
 
 class Maxent(object):
+    """
+    注意这里面最大的特征的数量是mxn, 但是实际上这个mxn的矩阵会非常稀疏.
+    """
     def __init__(self, tol=1e-4, max_iter=100):
         self.X_ = None
         self.y_ = None
         self.m = None        # 类别数量
-        self.n = None        # 特征数量
+        self.n = None        # 词表的数量
         self.N = None        # N 训练集样本容量
         self.M = None
         self.coef_ = None
@@ -28,7 +32,8 @@ class Maxent(object):
 
     def _px_pxy(self, x, y):
         """
-        统计TF, 这里面没有用稀疏存储的方式.
+        统计TF, 这里面没有用稀疏存储的方式. 所以这里会有很多的0, 包括后面的E也会有很多零, 需要处理掉除零的问题.
+        这里x, y是全量的数据,
         :param x:
         :param y:
         :return:
@@ -37,12 +42,11 @@ class Maxent(object):
         self.Px = np.zeros(self.n)
 
         # 相当于按照特征统计了
-        # 在这个例子里面, 相当于词表的大小是256, 也就是说特征就是灰度直方图
-        for idx in range(len(x)):
-            # 遍历每个样本
-            x_, y_ = x[idx], y[idx]
-            # 某个灰度值在对应的标签上的总数
-            for x__ in x_:
+        # 在这个例子里面, 相当于词表的大小是256, 对应的特征就是词表和类别组合
+
+        for x_, y_ in zip(x, y):
+            # 遍历每个样本, 某个灰度值在对应的标签上的总数, 注意每个样本中, 某个x__出现多少次的贡献认为都一样
+            for x__ in set(x_):
                 self.Pxy[self.label_names[y_], self.feature_names[x__]] += 1
                 self.Px[self.feature_names[x__]] += 1           # 某个灰度值的总数
         # 计算书中82页最下面那个期望
@@ -53,17 +57,19 @@ class Maxent(object):
     def _pw(self, x):
         """
         计算书85页公式6.22和6.23, 这个表示的是最大熵模型.
+        mask相当于给
         :param x:
         :return:
         """
-        mask = np.zeros(self.n)
-        print("x->", type(x), x)
+        mask = np.zeros(self.n+1)
+        # print("x->", type(x), x)
         for idx in x:
             mask[self.feature_names[idx]] = 1
-        tmp = self.coef_*mask
+        tmp = self.coef_*mask[1:]
         pw = np.exp(np.sum(tmp, axis=1))
         Z = np.sum(pw)
-        return pw/Z
+        pw = pw/Z
+        return pw
 
     def _EPx(self):
         """
@@ -87,14 +93,14 @@ class Maxent(object):
         :param y:
         :return: self: object
         """
-        self.N = len(x)  # 训练集大小
         self.X_ = x
-        self.y_ = set(y)
+        self.y_ = list(set(y))
         tmp = set(self.X_.flatten())
-        self.feature_names = defaultdict(int, zip(tmp, range(len(tmp))))
+        self.feature_names = defaultdict(int, zip(tmp, range(1, len(tmp)+1)))   # 从1开始编码
         self.label_names = dict(zip(self.y_, range(len(self.y_))))
-        self.n = len(self.feature_names)
+        self.n = len(self.feature_names)+1  # for default 0
         self.m = len(self.label_names)
+        self.N = len(x)  # 训练集大小
 
         self._px_pxy(x, y)
 
@@ -105,8 +111,12 @@ class Maxent(object):
             logger.info('iterate times %d' % i)
             # sigmas = []
             self._EPx()
-            self.M = 1000.0  # 书91页那个M，但实际操作中并没有用那个值
-            sigmas = 1/self.M*np.log(self.EPxy/self.EPx)
+            self.M = 1000  # 书91页那个M，但实际操作中并没有用那个值
+            with np.errstate(divide='ignore', invalid='ignore'):
+                tmp = np.true_divide(self.EPxy, self.EPx)
+                tmp[tmp == np.inf] = 0
+                tmp = np.nan_to_num(tmp)
+            sigmas = np.where(tmp != 0, 1/self.M*np.log(tmp), 0)
             self.coef_ = self.coef_ + sigmas
             i += 1
         return self
@@ -117,12 +127,12 @@ class Maxent(object):
         :param x:
         :return:
         """
-        rst = np.zeros(len(x))
+        rst = np.zeros(len(x), dtype=np.int64)
         for idx, x_ in enumerate(x):
             tmp = self._pw(x_)
-            print(tmp)
-            rst[idx] = self.label_names[np.argmax(tmp)]
-        return rst.astype(np.int64)
+            print(tmp, np.argmax(tmp), self.label_names)
+            rst[idx] = self.label_names[self.y_[np.argmax(tmp)]]
+        return np.array([self.y_[idx] for idx in rst])
 
     def predict_proba(self, x):
         """
@@ -137,25 +147,33 @@ class Maxent(object):
         return rst
 
 
+def load_data(path=None):
+    if path is None:
+        from sklearn.datasets import load_digits
+        raw_data = load_digits()
+        imgs, labels = raw_data.data, raw_data.target
+    else:
+        raw_data = pd.read_csv(path, sep="[,\t]", header=0)
+        data = raw_data.values
+        imgs, labels = data[0::, 1::], data[::, 0]
+    return imgs, labels
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
 
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-i", "--image", required=True,
-    #                 help="path to input image")
-    # args = vars(ap.parse_args())
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--path", required=False, help="path to input data")
+    args = vars(ap.parse_args())
 
     logger.info('Start read data')
     time_1 = time.time()
-    raw_data = pd.read_csv('./Input/sub_train_binary.csv', sep=",", header=0)
-    data = raw_data[:100].values
-
-    imgs = data[0::, 1::]
-    labels = data[::, 0]
-
+    imgs, labels = load_data(args["path"])
     train_features, test_features, train_labels, test_labels = train_test_split(imgs, labels,
-                                                                                test_size=0.33, random_state=23323)
+                                                                                test_size=0.33,
+                                                                                random_state=23323,
+                                                                                stratify=labels)
 
     logger.info("train test features %d, %d, %s" % (len(train_features), len(test_features), train_features[0]))
     time_2 = time.time()
@@ -169,9 +187,11 @@ if __name__ == '__main__':
     logger.info('training cost %f second' % (time_3 - time_2))
     logger.info('Start predicting')
     test_predict = met.predict(test_features)
+    print(test_labels, test_predict)
     time_4 = time.time()
     logger.info('predicting cost %d second' % (time_4 - time_3))
     score = accuracy_score(test_labels, test_predict)
     logger.info("The accruacy socre is %1.4f" % score)
+    # 全零数据
     rst = met.predict_proba([np.zeros(len(train_features[0]))])
     logger.info(rst)
