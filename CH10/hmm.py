3b5ed7e4c7099150d128ce5d8aa6babfd8fe9607
smirk <smirk.cao@gmail.com>
2018-09-18 01:26:57 +0800

    do forward

--- CH10/hmm.py
+++ CH10/hmm.py
@@ -4,6 +4,7 @@
 # Filename: hmm
 # Date: 9/17/18
 # Author: 😏 <smirk dot cao at gmail dot com>
+import numpy as np
 import argparse
 import logging
 import warnings
@@ -11,16 +12,32 @@ import warnings
 
 class HMM(object):
 
-    def __init__(self):
+    def __init__(self, n_component=0, Q=None, V=None):
         self.A = None
         self.B = None
         self.p = None
         self.M = 0
-        self.N = 0
+        self.N = n_component
         self.T = 0
+        self.Q = Q
+        self.V = V
 
-    def _do_forward(self):
-        pass
+    def _do_forward(self, X):
+        prob = 0
+        alpha = np.zeros((self.T, self.N))
+        # A: NxM
+        # B: NxM
+        # alpha: TxN
+        o = X[0]
+        alpha[:, 0] = self.p * self.B[:, o]
+        tmp = alpha[:, 0]
+        for k, o in enumerate(X[1:]):
+            alpha[:, k+1] = np.sum(tmp*self.A.T, axis=1)*self.B[:, o]
+            if k+1 < len(X):
+                tmp = alpha[:, k+1]
+        # prob = np.log(np.sum(alpha[:, k+1]))
+        prob = np.sum(alpha[:, k+1])
+        return prob, alpha
 
     def _do_backward(self):
         pass
--- CH10/unit_test.py
+++ CH10/unit_test.py
@@ -8,7 +8,6 @@ from hmm import *
 import numpy as np
 import pandas as pd
 import logging
-import warnings
 import unittest
 
 
@@ -90,6 +89,29 @@ class TestMEMethods(unittest.TestCase):
         logger.info(np.sum(np.dot(np.dot(pi*B[..., O[0]], A)*B[..., O[1]], A)*B[..., O[2]]))
         # backward
         logger.info(np.dot(A, B[..., O[2]]))
+
+    def test_forward(self):
+        # 10.2 数据
+        Q = {0: 1, 1: 2, 2: 3}
+        V = {0: "red", 1: "white"}
+        hmm_forward = HMM(n_component=3)
+        hmm_forward.A = np.array([[0.5, 0.2, 0.3],
+                                  [0.3, 0.5, 0.2],
+                                  [0.2, 0.3, 0.5]])
+        hmm_forward.B = np.array([[0.5, 0.5],
+                                  [0.4, 0.6],
+                                  [0.7, 0.3]])
+        hmm_forward.p = np.array([0.2, 0.4, 0.4])
+        X = np.array([0, 1, 0])
+        hmm_forward.T = len(X)
+
+        prob, alpha = hmm_forward._do_forward(X)
+        alpha_true = np.array([[0.10, 0.077, 0.04187],
+                               [0.16, 0.1104, 0.03551],
+                               [0.28, 0.0606, 0.05284]])
+        self.assertAlmostEqual(prob, 0.13022, places=5)
+        for x, y in zip(alpha_true.flatten().tolist(), alpha.flatten().tolist()):
+            self.assertAlmostEqual(x, y, places=5)
 
 
 if __name__ == '__main__':
