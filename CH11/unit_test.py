53ff6a6602014c482a661c69533008de8bc15d52
smirk <smirk.cao@gmail.com>
2018-09-21 02:17:33 +0800

    -

--- CH11/unit_test.py
+++ CH11/unit_test.py
@@ -7,6 +7,7 @@
 import argparse
 import logging
 import warnings
+import unittest
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
