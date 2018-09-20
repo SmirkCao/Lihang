391125016ddb82b638eaf293cafb513ab771f6c8
smirk <smirk.cao@gmail.com>
2018-09-21 02:15:30 +0800

    crf init

---
+++ CH11/unit_test.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+#! -*- coding=utf-8 -*-
+# Project:  Lihang
+# Filename: unit_test
+# Date: 9/21/18
+# Author: 😏 <smirk dot cao at gmail dot com>
+import argparse
+import logging
+import warnings
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--path", required=False, help="path to input data file")
+    args = vars(ap.parse_args())
+ 
