0219d9b969b0c85b6ccb2e1895d254874ffd589b
smirk <smirk.cao@gmail.com>
2018-09-26 20:46:30 +0800

    代码片段

--- CH11/crf.py
+++ CH11/crf.py
@@ -4,23 +4,84 @@
 # Filename: crf
 # Date: 9/21/18
 # Author: 😏 <smirk dot cao at gmail dot com>
+import numpy as np
 import argparse
 import logging
-import warnings
 
 
 class CRF(object):
-    def __init__(self):
+    def __init__(self, y=[]):
+        """
+        y: 标记的集合
+        """
+        self.alpha = None
+        self.beta = None
+        self.w_k = None
+        self.f_k = None
+        self.M = None
+        self.K = 0
+        self.y = y
+        self.m = len(y)
+        self.n = 0
+        self.f = None
+        self.Z = None
+
+    def fit(self, X):
+        self.n = len(X)
+        self.w_k = np.zeros(self.K)
+
+    def predict(self, X):
+        _, seq = self.decode(X)
+        return seq
+
+    def predict_proba(self, X):
         pass
 
-    def fit(self):
-        pass
-
-    def predict(self):
-        pass
-
-    def decode(self):
+    def decode(self, X):
         # 类似序列标注
+        prob = None
+        seq = None
+        return prob, seq
+
+    def _f_gen(self):
+        # self.f
+        # from template to generate the feature function
+        return self
+
+    def _calc_f_k(self):
+        self.M = np.array((self.m, self.m, self.K))
+
+        # 实际上f_k应该是m*m*k的矩阵
+        # i -> i-1
+        # j -> i
+        for k in range(self.K):
+            for i, y_i in enumerate(self.y):
+                for j, y_j in enumerate(self.y):
+                    self.M[i, j, :] = self.f[k](y_i, y_j, x, j)
+
+        return self
+
+    def _calc_M(self):
+
+        self.M = None
+        self.M = np.exp(np.sum(self.w_k*self.f_k))
+        return self
+
+    def _do_forward(self, X):
+        y0 = self.y[0]
+        self.alpha[0] = y0
+        for i in range(1, self.n+1):
+            self.alpha[i] = self.alpha[i-1]*self.M[i]
+        return self
+
+    def _do_backward(self, X):
+        yn1 = self.y[0]
+        self.beta[-1] = yn1
+        for i in range(self.n+1, 0, -1):
+            self.beta[i] = self.M[i+1]*self.beta[i+1]
+        return self
+
+    def _virtebi(self, X):
         pass
 
 
--- CH11/unit_test.py
+++ CH11/unit_test.py
@@ -9,14 +9,14 @@ from sympy import *
 import numpy as np
 import argparse
 import logging
-import warnings
 import unittest
+import re
 
 
 class TestCRF(unittest.TestCase):
     def test_e111(self):
-        # 针对11.1这个问题, 为什么要求非规范话的条件概率?
-        # 参考下书中预测算法部分
+        # Q:针对11.1这个问题, 为什么要求非规范话的条件概率?
+        # A:参考下书中预测算法部分
         Y = np.array([1, 2, 2])
         # 5 + 4 = 9
         w_k = np.array([1, 0.6, 1, 1, 0.2, 1, 0.5, 0.8, 0.5])
@@ -24,12 +24,14 @@ class TestCRF(unittest.TestCase):
         # todo: 注意联系最后的公式， 思考构建一个特征函数需要的参数与格式
         f_k = np.zeros(9)
         # transition feature
+        # i-1, i
         f_k[0] = np.sum([1 if tmp[0] == 1 and tmp[1] == 2 else 0 for tmp in list(zip(Y[:-1], Y[1:]))])
         f_k[1] = np.sum([1 if tmp[0] == 1 and tmp[1] == 1 else 0 for tmp in list(zip(Y[:-1], Y[1:]))])
         f_k[2] = np.sum([1 if tmp[0] == 2 and tmp[1] == 1 else 0 for tmp in list(zip(Y[:-1], Y[1:]))])
         f_k[3] = np.sum([1 if tmp[0] == 2 and tmp[1] == 1 else 0 for tmp in list(zip(Y[:-1], Y[1:]))])
         f_k[4] = np.sum([1 if tmp[0] == 2 and tmp[1] == 2 else 0 for tmp in list(zip(Y[:-1], Y[1:]))])
         # state feature
+        # i
         f_k[5] = np.sum([1 if tmp == 1 else 0 for tmp in [Y[0]]])
         f_k[6] = np.sum([1 if tmp == 2 else 0 for tmp in Y[:2]])
         f_k[7] = np.sum([1 if tmp == 1 else 0 for tmp in Y[1:]])
@@ -37,7 +39,7 @@ class TestCRF(unittest.TestCase):
 
         # 生成全局特征向量
         proba = np.sum(w_k*f_k)
-        logger.info("P(y|x) proportional to %1.1f" % proba)
+        logger.info("P(y|x) proportional to exp(%1.1f)" % proba)
         self.assertAlmostEqual(proba, 3.2, places=2)
 
     def test_e112(self):
@@ -55,6 +57,11 @@ class TestCRF(unittest.TestCase):
                      [1, 0]])
         Z = (M1 * M2 * M3 * M4)[0].expand()
         P = str(Z).replace(" ", "").split("+")
+        # 体会各个路径之间关系
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                    logger.info(str(M1[0, i] * M2[i, j] * M3[j, k]))
         logger.info(Z)
         logger.info(P)
         self.assertSetEqual(set(P),
@@ -63,6 +70,18 @@ class TestCRF(unittest.TestCase):
 
     def test_e113(self):
         pass
+
+    def test_readtemplate(self):
+        # 要清楚特征函数模板和特征函数不是一个概念
+        tpl = "\[(.*?)\]"
+        regex = re.compile(tpl)
+
+        with open("./Input/template") as f:
+            for line in f:
+                if line[0] == 'U':
+                    raw = line.strip().split(":")
+                    print(raw[0])
+                    print(regex.findall(raw[1]))
 
 
 if __name__ == '__main__':
