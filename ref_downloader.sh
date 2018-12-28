bad767181f4b3c567796359e4078990675757bf4
Smirk <smirk.cao@gmail.com>
2018-12-29 03:08:44 +0800

    wget link -O filename; 0809.pdf

--- ref_downloader.sh
+++ ref_downloader.sh
@@ -2,11 +2,11 @@ mkdir refs
 cd refs
 echo "ch08"
 # 0801
-wget https://cseweb.ucsd.edu/~yfreund/papers/IntroToBoosting.pdf
-mv IntroToBoosting.pdf 0801.pdf
+wget https://cseweb.ucsd.edu/~yfreund/papers/IntroToBoosting.pdf -O 0801.pdf
+#0807
+wget https://statweb.stanford.edu/~jhf/ftp/trebst.pdf -O 0807.pdf
 # 0808
-wget "https://sci2s.ugr.es/keel/pdf/algorithm/articulo/1999-ML-Improved%20boosting%20algorithms%20using%20confidence-rated%20predictions%20(Schapire%20y%20Singer).pdf"
-mv '1999-ML-Improved boosting algorithms using confidence-rated predictions (Schapire y Singer).pdf' 0808.pdf
-
-
+wget "https://sci2s.ugr.es/keel/pdf/algorithm/articulo/1999-ML-Improved%20boosting%20algorithms%20using%20confidence-rated%20predictions%20(Schapire%20y%20Singer).pdf" -O 0808.pdf
+# 0809
+wget "https://link.springer.com/content/pdf/10.1023%2FA%3A1013912006537.pdf" -O 0809.pdf 
 
