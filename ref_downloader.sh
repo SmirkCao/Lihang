7e4970615ae2ff78278a7d348a2a40f6ad1f6b24
Smirk <smirk.cao@gmail.com>
2019-01-09 10:40:35 +0800

    ref 0901

--- ref_downloader.sh
+++ ref_downloader.sh
@@ -11,4 +11,5 @@ wget https://statweb.stanford.edu/~jhf/ftp/trebst.pdf -O 0807.pdf
 wget "https://sci2s.ugr.es/keel/pdf/algorithm/articulo/1999-ML-Improved%20boosting%20algorithms%20using%20confidence-rated%20predictions%20(Schapire%20y%20Singer).pdf" -O 0808.pdf
 # 0809
 wget "https://link.springer.com/content/pdf/10.1023%2FA%3A1013912006537.pdf" -O 0809.pdf 
-
+# 0901
+wget "http://web.mit.edu/6.435/www/Dempster77.pdf" -O 0901.pdf
