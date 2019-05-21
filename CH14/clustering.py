890001429651f8ac23dbcf619c2cd99f651890b0
Smirk <smirk.cao@gmail.com>
2019-05-21 17:14:24 +0800

    algo 14.1

--- CH14/clustering.py
+++ CH14/clustering.py
@@ -4,4 +4,77 @@
 # Filename: clustering
 # Date: 5/15/19
 # Author: 😏 <smirk dot cao at gmail dot com>
+import numpy as np
 
+
+class Cluster(object):
+    def __init__(self, name, data, linkage="single"):
+        self.linkage = linkage
+        self.name = name
+        self.data = data[np.newaxis, :] if len(data.shape) == 1 else data
+        assert len(self.data.shape) == 2
+
+    def __sub__(a, b):
+        distance = np.Inf
+        if a.linkage == "single":
+            for i in a.data:
+                for j in b.data:
+                    # dij
+                    dij = np.sqrt(np.sum((i-j)**2))
+                    if dij < distance:
+                        distance = dij
+        return distance
+
+    def __or__(a, b):
+        # rst = Cluster(a.name + "_" + b.name, 
+        #               np.concatenate((a.data, b.data), axis=1))
+        rst = Cluster(a.name + "_" + b.name, np.vstack((a.data, b.data)))
+        return rst 
+
+    def __str__(self, ):
+        return "name: " + self.name + " data: " + str(self.data)
+
+
+class Clustering(object):
+    def __init__(self, k=2):
+        self.labels = None
+        self.k = k
+        self.d = None
+        self.metrics = None
+        self.gs = None
+
+    def fit(self, x):
+        pass
+
+    def predict(self, x):
+        pass
+
+
+class ClusterAgglomerative(Clustering):
+    # algo 14.1
+    def fit(self, x):
+        n_samples = x.shape[0]
+        self.labels = np.arange(n_samples)
+        # assign cluster per sample
+        gs = [Cluster(str(idx), data) for idx, data in enumerate(x)]
+        print(len(gs), self.k)
+        while len(gs) > self.k:
+            mindistance = np.inf
+            ga = None
+            gb = None
+            for g in gs:
+                for g_ in gs:
+                    if g == g_:
+                        continue
+                    distance = g - g_
+                    if distance < mindistance:
+                        ga = g
+                        gb = g_
+                        mindistance = distance
+            if mindistance < np.inf:
+                gs.remove(ga)
+                gs.remove(gb)
+                gs.append(ga | gb)
+                # print(ga, gb, mindistance)
+            # print("len of gs:", len(gs))
+        self.gs = gs
--- CH14/unit_test.py
+++ CH14/unit_test.py
@@ -1,6 +1,57 @@
 #! /usr/bin/env python
-#! -*- coding=utf-8 -*-
+#!-*- coding=utf-8 -*-
 # Project:  Lihang
 # Filename: unit_test
 # Date: 5/15/19
 # Author: 😏 <smirk dot cao at gmail dot com>
+
+# for vscode use
+# Ref to : https://code.visualstudio.com/docs/python/unit-testing
+import unittest
+from clustering import ClusterAgglomerative, Cluster
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class TestClusteringMethods(unittest.TestCase):
+    def test_e14_2(self):
+        data = [np.array([[0, 2], [0, 0], [1, 0], [5, 0], [5, 2]]), np.array([0, 1, 1, 1, 0])]
+        clustering = ClusterAgglomerative(k=2)
+        clustering.fit(data[0])
+        print(data[0]),
+        print(data[1])
+        print(clustering.gs[0].data)
+        print(clustering.gs[1].data)
+
+        plt.scatter(clustering.gs[0].data[:, 0],
+                    clustering.gs[0].data[:, 1])
+        plt.scatter(clustering.gs[1].data[:, 0],
+                    clustering.gs[1].data[:, 1])
+        plt.show()
+
+    def test_fit(self):
+        n_samples = 150
+        data = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
+        clustering = ClusterAgglomerative(k=2)
+        clustering.fit(data[0])
+        print(data[0]),
+        print(data[1])
+        print(clustering.gs[0].data)
+        print(clustering.gs[1].data)
+
+        plt.scatter(clustering.gs[0].data[:, 0],
+                    clustering.gs[0].data[:, 1])
+        plt.scatter(clustering.gs[1].data[:, 0],
+                    clustering.gs[1].data[:, 1])
+        plt.show()
+
+    def test_cluster(self):
+        a = Cluster("a", np.array([[2], [2], [4]]))
+        b = Cluster("b", np.array([[4], [5], [6]]))
+        self.assertEqual(a-b, 0)
+        print(a | b | b | a | b)
+
+
+if __name__ == "main":
+    unittest.main()
