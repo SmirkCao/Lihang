301529b1a9b14290cf64b2f8f125310b2df19065
Smirk <smirk.cao@gmail.com>
2019-05-28 16:50:32 +0800

    CH17 init

---
+++ CH17/unit_test.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+#!-*- coding=utf-8 -*-
+# Project:  Lihang
+# Filename: unit_test
+# Date: 5/28/19
+# Author: 😏 <smirk dot cao at gmail dot com>
+
+# for vscode use
+# Ref to : https://code.visualstudio.com/docs/python/unit-testing
+import unittest
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class TestLSAMethods(unittest.TestCase):
+    pass
